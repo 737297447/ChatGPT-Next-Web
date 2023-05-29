ffec6d37427f8b63f2535b1c4eb00039dc86d15d
志龙 <737297447@users.noreply.github.com>
2023-05-29 16:01:49 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -121,7 +121,7 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>零一爱-GPT</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          技术支持：15802907461
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
